ec24d7f6969c74c1a3d14f6e29fc5f30a8286ee9
Paweł Krawczyk <pawel.krawczyk@interia.com>
2021-07-13 17:03:24 +0200

    adding information cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,17 +9,26 @@ module.exports = {
     textColor:{
       'dark': '#1e1e1e',
       'white': 'white',
+      'font-light': '#777777',
+      'pink': '#ff589e'
     },
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'pink': '#ff589e'
+     }),
     translate: {
-      'm55': '-55%'
+      'nt55': '-55%'
     },
     lineHeight:{
       '16':'4rem',
       '12':'3rem',
-      '8':'2rem'
+      '8':'2rem',
+      '7':'1.75rem'
     },
-
-
+    zIndex:{
+      'n1':'-1',
+      '10':'10'
+    },
     extend: {
       backgroundImage: theme => ({
         'welcome': "url('/dist/images/banner-bg.png')",
